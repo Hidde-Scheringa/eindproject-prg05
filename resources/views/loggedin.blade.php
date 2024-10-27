3fd23047fb27402dc0f798e62dbc498b7fb3ff38
Hidde-Scheringa <hescheringa@gmail.com>
2024-10-27 14:24:42 +0100

    zoekbalk weer toegevoegd

--- resources/views/loggedin.blade.php
+++ resources/views/loggedin.blade.php
@@ -18,7 +18,20 @@
 <div class="container mx-auto py-10">
     <h2 class="text-2xl font-semibold text-center text-gray-700 mb-6">Welkom bij je dashboard! Hier kun je je spellen beheren.</h2>
 
-    <!-- Genre Filter Knoppen -->
+    <form class="max-w-lg mx-auto my-10">
+        @csrf
+        <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+        <div class="relative">
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 19l-4-4m0-7a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+                </svg>
+            </div>
+            <input type="search" id="search" name="search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+            <button type="submit" class="absolute right-2.5 bottom-2.5 bg-blue-600 text-white hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+        </div>
+    </form>
+
     <div class="flex justify-center space-x-4 mb-8">
         @foreach($genres as $genre)
             <a href="{{ route('games.genrefilter', ['genre_id' => $genre->id]) }}">
